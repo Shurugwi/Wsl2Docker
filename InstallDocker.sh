082882dfa4d62b58ff6d44ca6463296730c98f92
Shurugwi <natielocke@gmail.com>
2021-07-03 04:56:41 +0100

    Script updates

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -2,15 +2,15 @@
 
 echo "Installing Docker"
 
-apk del docker-cli docker-engine docker-openrc docker-compose docker
+apk del docker-cli docker-engine docker-openrc docker-compose docker shadow curl
 apk update
 apk upgrade -U
 
 apk add docker --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
 addgroup $USER docker
 
-#sed -i -e 's/^\(docker:x\):[^:]\+/\1:36257/' /etc/group
-#groupmod -g 36257 docker
+sed -i -e 's/^\(docker:x\):[^:]\+/\1:36257/' /etc/group
+groupmod -g 36257 docker
 
 DOCKER_DIR=/mnt/wsl/shared-docker
 mkdir -pm o=,ug=rwx "$DOCKER_DIR"
@@ -25,13 +25,13 @@ tee -a /etc/docker/daemon.json <<EOF
 EOF
 
 tee -a ~/.profile <<EOF
-DOCKER_DISTRO="LocalDockerHost"
-DOCKER_DIR=/mnt/wsl/shared-docker
-DOCKER_SOCK="$DOCKER_DIR/docker.sock"
-export DOCKER_HOST="unix://$DOCKER_SOCK"
-if [ ! -S "$DOCKER_SOCK" ]; then
-    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
-    chgrp docker "$DOCKER_DIR"
-    /mnt/c/Windows/System32/wsl.exe -d $DOCKER_DISTRO sh -c "nohup dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1" &
+export DOCKER_HOST="unix:///mnt/wsl/shared-docker/docker.sock"
+if [ ! -S "/mnt/wsl/shared-docker/docker.sock" ]; then
+    mkdir -pm o=,ug=rwx /mnt/wsl/shared-docker
+    chgrp docker /mnt/wsl/shared-docker
+fi
+
+if [ ! psgrep dockerd > 0 ]; then
+    /mnt/c/Windows/System32/wsl.exe -d LocalDockerHost sh -c "nohup dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1" &
 fi
 EOF
