602e8bb87983d92c3630c3efcc362edf5f385457
Shurugwi <natielocke@gmail.com>
2021-07-13 10:54:22 +0100

    Update Wsl2Docker.ps1

--- Wsl2Docker.ps1
+++ Wsl2Docker.ps1
@@ -174,7 +174,7 @@ else
     $wsloutput = & wsl --import LocalDockerHost $DefaultWslFolder $AlpineImageFileName
     $wsloutput
     & wsl -l -v
-    & wsl -d LocalDockerHost -e sh -c "exit"
+    & wsl -d LocalDockerHost -e sh -c "echo '185.199.109.133 raw.githubusercontent.com' >> /etc/hosts && exit"
     & wsl -d LocalDockerHost -e sh -c "echo 'Attempting to download installation script...' && echo 'nameserver 8.8.8.8' >> /etc/resolv.conf && wget -q https://raw.githubusercontent.com/Shurugwi/Wsl2Docker/main/InstallDocker.sh -O - | ash && exit"
 }
 
